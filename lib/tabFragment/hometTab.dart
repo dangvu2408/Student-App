e33934dadf148e3bcaf20525119733bb50e18e94
dangvu2408 <hiend6887@gmail.com>
2024-06-13 23:09:28 +0700

    layout (13-06-2024)

--- lib/tabFragment/hometTab.dart
+++ lib/tabFragment/hometTab.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
 
-class homeTab extends StatelessWidget {
+class homeTab extends StatefulWidget {
+    homeTab({Key? key}) : super(key: key);
+
+    @override
+    State<homeTab> createState() => homeStateWidget();
+}
+
+class homeStateWidget extends State<homeTab> {
+    ScrollController controller = ScrollController();
+    bool showOverlay = false;
+
+    @override
+    void initState() {
+        super.initState();
+        controller.addListener(() {
+            if (controller.offset >= 100) {
+                if (!showOverlay) {
+                    setState(() {
+                        showOverlay = true;
+                    });
+                }
+            } else {
+                if (showOverlay) {
+                    setState(() {
+                        showOverlay = false;
+                    });
+                }
+            }
+        });
+    }
+
+    @override
+    void dispose() {
+        controller.dispose();
+        super.dispose();
+    }
     @override
     Widget build(BuildContext context) {
         return Scaffold(
@@ -15,7 +50,7 @@ class homeTab extends StatelessWidget {
                         ),
                     ),
                     Container(
-                        margin: const EdgeInsets.only(top: 25, right: 25),
+                        margin: const EdgeInsets.only(top: 15, right: 25),
                         child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
@@ -40,23 +75,54 @@ class homeTab extends StatelessWidget {
                         ),
                     ),
                     SingleChildScrollView(
-                        child: Positioned(
-                            top: 0,
-                            left: 0,
-                            right: 0,
-                            child: Container(
-                                height: MediaQuery.of(context).size.height,
-                                margin: const EdgeInsets.only(top: 180),
-                                decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(25.0),
-                                        topRight: Radius.circular(25.0),
-                                    ),
+                        controller: controller,
+                        child: Container(
+                            height: MediaQuery.of(context).size.height,
+                            margin: const EdgeInsets.only(top: 180),
+                            decoration: const BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(25.0),
+                                    topRight: Radius.circular(25.0),
                                 ),
-                            )
-                        ),
+                            ),
+                        )
                     ),
+                    Positioned(
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        child: AnimatedOpacity(
+                            opacity: showOverlay ? 1.0 : 0.0,
+                            duration: const Duration(milliseconds: 300),
+                            child: Container(
+                                padding: const EdgeInsets.only(top: 15, right: 25),
+                                color: Color(0xFFD80015),
+                                child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                        Flexible(
+                                            child: Container(
+                                                width: double.infinity,
+                                                height: 90,
+                                                child: const Image(
+                                                    image: AssetImage('assets/images/logofull.png'),
+                                                ),
+                                            ),
+                                        ),
+                                        Flexible(
+                                            child: Container(
+                                                width: 25,
+                                                child: const Image(
+                                                    image: AssetImage('assets/images/settingicon.png'),
+                                                ),
+                                            ),
+                                        )
+                                    ],
+                                ),
+                            ),
+                        ),
+                    )
                 ],
             ),
         );
