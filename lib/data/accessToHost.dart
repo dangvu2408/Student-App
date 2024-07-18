62e8ec290c045b7cbb89a2a9ab48c4ca7b788d8d
dangvu2408 <hiend6887@gmail.com>
2024-07-18 16:21:35 +0700

    login properties (be) 18072024

---
+++ lib/data/accessToHost.dart
@@ -0,0 +1,121 @@
+
+
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' as htmlParser;
+import 'package:crypto/crypto.dart';
+import 'package:hust_sa/navigator/loginProperties.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../main.dart';
+
+class accessToHost {
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+    TextEditingController captchaController = TextEditingController();
+    Uint8List? captchaImage;
+    String? captchaImageUrl;
+    String? viewState;
+    String? eventValidation;
+    Map<String, String> cookies = {};
+
+    void _showToast(String message) {
+        Fluttertoast.showToast(
+            msg: message,
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.BOTTOM,
+        );
+    }
+
+    Future<void> loadCaptcha(Function setState) async {
+        try {
+            final response = await http.get(Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'));
+            if (response.statusCode == 200) {
+                final document = htmlParser.parse(response.body);
+                final captchaElement = document.getElementById('ctl00_ctl00_contentPane_MainPanel_MainContent_ASPxCaptcha1_IMG');
+                if (captchaElement != null) {
+                    final srcValue = captchaElement.attributes['src'];
+                    if (srcValue != null) {
+                        setState(() {
+                            captchaImageUrl = 'https://ctt-sis.hust.edu.vn' + srcValue;
+                            viewState = document.getElementById('__VIEWSTATE')?.attributes['value'];
+                            eventValidation = document.getElementById('__EVENTVALIDATION')?.attributes['value'];
+                        });
+                    } else {
+                        _showToast('Lỗi: src của phần tử captcha là null');
+                    }
+                } else {
+                    _showToast('Lỗi: src của phần tử captcha là null');
+                }
+            } else {
+                _showToast('Lỗi tải captcha');
+            }
+        } catch (e) {
+            _showToast('Error: $e');
+        }
+    }
+
+    String _md5(String input) {
+        var bytes = utf8.encode(input);
+        var digest = md5.convert(bytes);
+        return digest.toString();
+    }
+
+    Future<void> login(Function setState) async {
+        if (usernameController.text.isEmpty ||
+                passwordController.text.isEmpty ||
+                captchaController.text.isEmpty) {
+            Fluttertoast.showToast(msg: 'Vui lòng điền vào trường còn thiếu');
+            return;
+        }
+
+        final username = usernameController.text;
+        final password = passwordController.text;
+        final captcha = captchaController.text;
+        final md5Hash = _md5('$username.$password');
+        final loginData = {
+            'ma_hoa': md5Hash,
+            'username': username,
+            'password': password,
+            'captcha': captcha,
+        };
+
+        final success = await _performLogin(loginData);
+
+
+        if (success) {
+            _saveUser();
+            _showToast('Đăng nhập thành công!');
+            Navigator.pushReplacement(
+                loginActivity() as BuildContext,
+                MaterialPageRoute(builder: (loginActivity) => myWidget(title: 'HUST')),
+            );
+        } else {
+            _showToast('Đăng nhập thất bại!');
+            await loadCaptcha(setState);
+        }
+    }
+
+    Future<bool> _performLogin(Map<String, String> loginData) async {
+        try {
+            final response = await http.post(
+                Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'),
+                body: loginData,
+            );
+            return response.statusCode == 200;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    Future<void> _saveUser() async {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('username', usernameController.text);
+        await prefs.setString('password', passwordController.text);
+    }
+}
--- lib/navigator/loginProperties.dart
+++ lib/navigator/loginProperties.dart
@@ -8,13 +8,12 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hust_sa/main.dart';
-import 'package:hust_sa/tabFragment/hometTab.dart';
-import 'package:page_transition/page_transition.dart';
 import 'dart:typed_data';
 import 'package:crypto/crypto.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as htmlParser;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:hust_sa/data/accessToHost.dart';
 class loginActivity extends StatefulWidget {
     @override
     loginState createState() => loginState();
@@ -22,17 +21,17 @@ class loginActivity extends StatefulWidget {
 
 class loginState extends State<loginActivity> {
     int currentStep = 0;
-    TextEditingController usernameController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    TextEditingController captchaController = TextEditingController();
-    Uint8List? captchaImage;
-    String? captchaImageUrl;
-    String? viewState;
-    String? eventValidation;
-    Map<String, String> cookies = {};
+    // TextEditingController usernameController = TextEditingController();
+    // TextEditingController passwordController = TextEditingController();
+    // TextEditingController captchaController = TextEditingController();
+    // Uint8List? captchaImage;
+    // String? captchaImageUrl;
+    // String? viewState;
+    // String? eventValidation;
+    // Map<String, String> cookies = {};
     void initState() {
         super.initState();
-        loadCaptcha();
+        accessToHost().loadCaptcha(setState);
     }
 
     continueStep() {
@@ -42,7 +41,7 @@ class loginState extends State<loginActivity> {
             });
         }
         else {
-            login();
+            accessToHost().login(setState);
         }
     }
     cancelStep() {
@@ -113,101 +112,101 @@ class loginState extends State<loginActivity> {
         );
     }
 
-    void _showToast(String message) {
-        Fluttertoast.showToast(
-            msg: message,
-            toastLength: Toast.LENGTH_LONG,
-            gravity: ToastGravity.BOTTOM,
-        );
-    }
-
-    Future<void> loadCaptcha() async {
-        try {
-            final response = await http.get(Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'));
-            if (response.statusCode == 200) {
-                final document = htmlParser.parse(response.body);
-                final captchaElement = document.getElementById('ctl00_ctl00_contentPane_MainPanel_MainContent_ASPxCaptcha1_IMG');
-                if (captchaElement != null) {
-                    final srcValue = captchaElement.attributes['src'];
-                    if (srcValue != null) {
-                        setState(() {
-                            captchaImageUrl = 'https://ctt-sis.hust.edu.vn' + srcValue;
-                            viewState = document.getElementById('__VIEWSTATE')?.attributes['value'];
-                            eventValidation = document.getElementById('__EVENTVALIDATION')?.attributes['value'];
-                        });
-                    } else {
-                        _showToast('Lỗi: src của phần tử captcha là null');
-                    }
-                } else {
-                    _showToast('Không tìm thấy phần tử captcha');
-                }
-            } else {
-                _showToast('Lỗi tải captcha');
-            }
-        } catch (e) {
-            _showToast('Error: $e');
-        }
-    }
-
-    String _md5(String input) {
-        var bytes = utf8.encode(input);
-        var digest = md5.convert(bytes);
-        return digest.toString();
-    }
-
-    Future<void> login() async {
-        if (usernameController.text.isEmpty ||
-            passwordController.text.isEmpty ||
-            captchaController.text.isEmpty) {
-            Fluttertoast.showToast(msg: 'Vui lòng điền vào trường còn thiếu');
-            return;
-        }
-
-        final username = usernameController.text;
-        final password = passwordController.text;
-        final captcha = captchaController.text;
-        final md5Hash = _md5('$username.$password');
-        final loginData = {
-            'ma_hoa': md5Hash,
-            'username': username,
-            'password': password,
-            'captcha': captcha,
-        };
-
-        final success = await _performLogin(loginData);
-
-        Navigator.of(context).pop();
-
-        if (success) {
-            _saveUser();
-            _showToast('Đăng nhập thành công!');
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => myWidget(title: 'HUST')),
-            );
-        } else {
-            _showToast('Đăng nhập thất bại!');
-            await loadCaptcha();
-        }
-    }
-
-    Future<bool> _performLogin(Map<String, String> loginData) async {
-        try {
-            final response = await http.post(
-                Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'),
-                body: loginData,
-            );
-            return response.statusCode == 200;
-        } catch (e) {
-            return false;
-        }
-    }
-
-    Future<void> _saveUser() async {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setString('username', usernameController.text);
-        await prefs.setString('password', passwordController.text);
-    }
+    // void _showToast(String message) {
+    //     Fluttertoast.showToast(
+    //         msg: message,
+    //         toastLength: Toast.LENGTH_LONG,
+    //         gravity: ToastGravity.BOTTOM,
+    //     );
+    // }
+    //
+    // Future<void> loadCaptcha() async {
+    //     try {
+    //         final response = await http.get(Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'));
+    //         if (response.statusCode == 200) {
+    //             final document = htmlParser.parse(response.body);
+    //             final captchaElement = document.getElementById('ctl00_ctl00_contentPane_MainPanel_MainContent_ASPxCaptcha1_IMG');
+    //             if (captchaElement != null) {
+    //                 final srcValue = captchaElement.attributes['src'];
+    //                 if (srcValue != null) {
+    //                     setState(() {
+    //                         captchaImageUrl = 'https://ctt-sis.hust.edu.vn' + srcValue;
+    //                         viewState = document.getElementById('__VIEWSTATE')?.attributes['value'];
+    //                         eventValidation = document.getElementById('__EVENTVALIDATION')?.attributes['value'];
+    //                     });
+    //                 } else {
+    //                     _showToast('Lỗi: src của phần tử captcha là null');
+    //                 }
+    //             } else {
+    //                 _showToast('Không tìm thấy phần tử captcha');
+    //             }
+    //         } else {
+    //             _showToast('Lỗi tải captcha');
+    //         }
+    //     } catch (e) {
+    //         _showToast('Error: $e');
+    //     }
+    // }
+    //
+    // String _md5(String input) {
+    //     var bytes = utf8.encode(input);
+    //     var digest = md5.convert(bytes);
+    //     return digest.toString();
+    // }
+    //
+    // Future<void> login() async {
+    //     if (usernameController.text.isEmpty ||
+    //         passwordController.text.isEmpty ||
+    //         captchaController.text.isEmpty) {
+    //         Fluttertoast.showToast(msg: 'Vui lòng điền vào trường còn thiếu');
+    //         return;
+    //     }
+    //
+    //     final username = usernameController.text;
+    //     final password = passwordController.text;
+    //     final captcha = captchaController.text;
+    //     final md5Hash = _md5('$username.$password');
+    //     final loginData = {
+    //         'ma_hoa': md5Hash,
+    //         'username': username,
+    //         'password': password,
+    //         'captcha': captcha,
+    //     };
+    //
+    //     final success = await _performLogin(loginData);
+    //
+    //     Navigator.of(context).pop();
+    //
+    //     if (success) {
+    //         _saveUser();
+    //         _showToast('Đăng nhập thành công!');
+    //         Navigator.pushReplacement(
+    //             context,
+    //             MaterialPageRoute(builder: (context) => myWidget(title: 'HUST')),
+    //         );
+    //     } else {
+    //         _showToast('Đăng nhập thất bại!');
+    //         await loadCaptcha();
+    //     }
+    // }
+    //
+    // Future<bool> _performLogin(Map<String, String> loginData) async {
+    //     try {
+    //         final response = await http.post(
+    //             Uri.parse('https://ctt-sis.hust.edu.vn/Account/Login.aspx'),
+    //             body: loginData,
+    //         );
+    //         return response.statusCode == 200;
+    //     } catch (e) {
+    //         return false;
+    //     }
+    // }
+    //
+    // Future<void> _saveUser() async {
+    //     final prefs = await SharedPreferences.getInstance();
+    //     await prefs.setString('username', usernameController.text);
+    //     await prefs.setString('password', passwordController.text);
+    // }
 
     @override
     Widget build(BuildContext context) {
@@ -259,7 +258,7 @@ class loginState extends State<loginActivity> {
                                                 height: 40,
                                                 margin: EdgeInsets.symmetric(vertical: 5),
                                                 child: TextField(
-                                                    controller: usernameController,
+                                                    controller: accessToHost().usernameController,
                                                     decoration: InputDecoration(
                                                         filled: true,
                                                         fillColor: Colors.white,
@@ -284,7 +283,7 @@ class loginState extends State<loginActivity> {
                                                 height: 40,
                                                 margin: EdgeInsets.symmetric(vertical: 5),
                                                 child: TextField(
-                                                    controller: passwordController,
+                                                    controller: accessToHost().passwordController,
                                                     obscureText: true,
                                                     decoration: InputDecoration(
                                                         filled: true,
@@ -313,7 +312,7 @@ class loginState extends State<loginActivity> {
                                                     children: [
                                                         Expanded(
                                                             child: TextField(
-                                                                controller: captchaController,
+                                                                controller: accessToHost().captchaController,
                                                                 keyboardType: TextInputType.number,
                                                                 decoration: InputDecoration(
                                                                     filled: true,
@@ -346,12 +345,13 @@ class loginState extends State<loginActivity> {
                                                             ),
                                                             child: Stack(
                                                                 children: [
-                                                                    Image.network(captchaImageUrl!),
+                                                                    if (accessToHost().captchaImageUrl != null)
+                                                                        Image.network(accessToHost().captchaImageUrl!),
                                                                     Align(
                                                                         alignment: Alignment.centerRight,
                                                                         child: GestureDetector(
                                                                             onTap: () {
-                                                                                loadCaptcha();
+                                                                                accessToHost().loadCaptcha(setState);
                                                                             },
                                                                             child: const Image(
                                                                                 height: 20,
